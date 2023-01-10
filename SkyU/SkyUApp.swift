13fb16ce477eff2c98637570b10c497918196500
Biswamohan-creator <56292855+Biswamohan-creator@users.noreply.github.com>
2023-01-11 00:03:20 +0530

    Initial Commit

---
+++ SkyU/SkyUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyUApp.swift
+//  SkyU
+//
+//  Created by CHINAM DWARIKANATH PATRA on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SkyUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
